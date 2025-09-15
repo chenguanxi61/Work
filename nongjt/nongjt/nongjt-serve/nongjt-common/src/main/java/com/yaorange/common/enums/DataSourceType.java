5a8d54c512c754f408e54945fe34e3f890db2198
chenguanxi61 <1923064449@qq.com>
2025-09-15 10:35:13 +0800

    初始项目

---
+++ nongjt/nongjt/nongjt-serve/nongjt-common/src/main/java/com/yaorange/common/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.yaorange.common.enums;
+
+/**
+ * 数据源
+ * 
+ * @author ruoyi
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
